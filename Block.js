d6416f2966ca0cd0fa4bde1b9519567e1ba98672
Pawan kumar <32352500+Pawan0411@users.noreply.github.com>
2018-09-23 10:26:30 +0530

    updated objects

--- Block.js
+++ Block.js
@@ -1,32 +1,26 @@
-let SHA256 = require("crypto-js/sha256");
-
 class Block {
-    constructor(index, previousHash, senderKey, recieverKey, data) {
-        this.index = index;
-        this.previousHash = previousHash;
-        this.timestamp = Math.floor(Date.now() / 1000);
-        this.data = data;
-        this.hash = this.calculateHash();
-        this.nonce = 0;
-        this.senderKey = senderKey;
-        this.recieverKey = recieverKey;
-    }
 
-    calculateHash() {
-        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
-    }
+  constructor(senderKey,recieverKey,message) {
 
-    mineBlock(difficulty) {
-        /**The array.join() method is an inbuilt function in JavaScript
-        which is used to join the elements of an array into a string.*/
-        while (this.hash.substring(0, difficulty) !== "0000") {
-            this.nonce++;
-            this.hash = this.calculateHash();
-        }
-        console.log("Prev Hash : " + this.previousHash);
-        console.log("BLOCK MINED: " + this.hash);
-        console.log("Nonce : " + this.nonce);
+    this.index = 0
+    this.previousHash = ""
+    this.hash = ""
+    this.nonce = 0
+    this.data={
+        recieverKey:recieverKey,
+        senderKey: senderKey,
+        message:message
     }
+    
 }
 
-module.exports = Block;
+  get key() {
+    return JSON.stringify(this.data) + this.index + this.previousHash + this.nonce
+  }
+
+  addData(data) {
+    this.data.push(data)
+  }
+}
+
+module.exports = Block
