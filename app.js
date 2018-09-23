888a8e417d6866bf4765528aaaa4962785b9b5e6
Pawan kumar <32352500+Pawan0411@users.noreply.github.com>
2018-09-23 10:25:47 +0530

    testing data object

--- app.js
+++ app.js
@@ -1,96 +1,106 @@
+
 const express = require('express'),
-    mongoose = require('mongoose'),
-    bodyParser = require('body-parser'),
-    cryptojs = require('crypto-js'),
-    User = require('./models/user.js');
+mongoose = require('mongoose'),
+bodyParser = require('body-parser'),
+cryptojs = require('crypto-js'),
+User = require('./models/user.js');
+
+let Block = require('./models/Block')
+let Data = require('./models/data')
+let Blockchain = require('./models/Blockchain')
+
+// create genesis block
+let genesisBlock = new Block('0','0','0');
+let blockchain = new Blockchain(genesisBlock)
 
 
 
 mongoose.connect('mongodb://localhost:27017/db', {
-    useNewUrlParser: true
+useNewUrlParser: true
 });
 
 var app = express();
-const Blockchain = require('./Blockchain');
-const blockchain = new Blockchain();
-console.log(blockchain)
+
+
 
 app.use(bodyParser.urlencoded({
-    extended: true
+extended: true
 }));
 
 app.post('/signup', (req, res) => {
-    User.findOne({
-        email: req.body.email
-    }, (err, user) => {
-        if (!err && user) {
-            res.send('User Exist')
-        } else {
-            let prvK = cryptojs.SHA256(Date.now());
-            let pubK = cryptojs.SHA256(prvK);
-            if (!user) {
-                let tmp = {
-                    email: req.body.email,
-                    name: req.body.name,
-                    hashedPassword: cryptojs.SHA256(req.body.password),
-                    privateKey: prvK,
-                    publicKey: pubK
-                }
-                User.create(tmp, (err, user) => {
-                    if (!err) {
-                        console.log('User created');
-                        console.log(user);
-                        res.send('User Created')
-                    } else {
-                        console.log('Error : ' + err);
-                        res.send('Error : ' + err);
-                    }
-                })
+User.findOne({
+    email: req.body.email
+}, (err, user) => {
+    if (!err && user) {
+        res.send('User Exist')
+    } else {
+        let prvK = cryptojs.SHA256(Date.now());
+        let pubK = cryptojs.SHA256(prvK);
+        if (!user) {
+            let tmp = {
+                email: req.body.email,
+                name: req.body.name,
+                hashedPassword: cryptojs.SHA256(req.body.password),
+                privateKey: prvK,
+                publicKey: pubK
             }
+            User.create(tmp, (err, user) => {
+                if (!err) {
+                    console.log('User created');
+                    console.log(user);
+                    res.send('User Created')
+                } else {
+                    console.log('Error : ' + err);
+                    res.send('Error : ' + err);
+                }
+            })
         }
-    })
+    }
+})
 })
 
 app.post('/login', (req, res) => {
-    User.findOne({
-        email: req.body.email
-    }, (err, user) => {
-        if (!err) {
-            if (user) {
-                if (cryptojs.SHA256(req.body.password) == user.hashedPassword) {
+User.findOne({
+    email: req.body.email
+}, (err, user) => {
+    if (!err) {
+        if (user) {
+            if (cryptojs.SHA256(req.body.password) == user.hashedPassword) {
+                res.send('login successfull');
 
-                    // console.log('Mining block 1...');
-                    // blockchain.addBlock(new Block(1, email));
-
-                    // console.log('Mining block 2...');
-                    // blockchain.addBlock(new Block(2, email));
-                    console.log('Successfully logged In');
-                    res.send('login successfull');
-
-                } else {
-                    console.log('incorrect password');
-                    res.send('Incorrect password');
-                }
             } else {
-                console.log('User not found');
-                res.send('User not found');
+                console.log('incorrect password');
+                res.send('Incorrect password');
             }
         } else {
-            console.log('Error : ' + err);
-            res.send('Error : ' + err);
+            console.log('User not found');
+            res.send('User not found');
         }
-    })
+    } else {
+        console.log('Error : ' + err);
+        res.send('Error : ' + err);
+    }
+})
 })
 
 app.post('/send', (req, res) => {
-    blockchain.addBlock(blockchain.chain.length + 1, blockchain.getLatestBlock().hash, req.body.senderKey, req.body.recieverKey, req.body.message);
-    res.send('Block sent');
+  
 });
 
 app.listen(3000, () => {
-    console.log('server started');
-
-
-
-
+console.log('server started');
 })
+
+/*
+blockchain.addBlock(block)
+
+
+let anotherdata = new Data("Azam","Jerry",10)
+let block1 = blockchain.getNextBlock([data])
+blockchain.addBlock(block1)
+
+let anotherdata1 = new Data("Azam","Jerry",10)
+let block2 = blockchain.getNextBlock([data])
+blockchain.addBlock(block2)
+*/
+console.log(blockchain.blocks[0].data)
